40fcd3c272b9e318c0368736e59880464a52ca1a
1337-ishaan <1337.ishaan@gmail.com>
2020-12-04 19:48:31 +0000

    Updates

---
+++ precache-manifest.915bca89deefa4ff7423b6b7ee953025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e821138b162f8322d7385c365f19b595",
+    "url": "/react-data-visualization/index.html"
+  },
+  {
+    "revision": "f5221dad8946dae76ed7",
+    "url": "/react-data-visualization/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cea67109cbc8e84d99f3",
+    "url": "/react-data-visualization/static/css/main.26c159cd.chunk.css"
+  },
+  {
+    "revision": "f5221dad8946dae76ed7",
+    "url": "/react-data-visualization/static/js/2.4fee6efc.chunk.js"
+  },
+  {
+    "revision": "825b3f1924aa91c0b22224e02cce27f9",
+    "url": "/react-data-visualization/static/js/2.4fee6efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea67109cbc8e84d99f3",
+    "url": "/react-data-visualization/static/js/main.5cedc0be.chunk.js"
+  },
+  {
+    "revision": "6cfb9256a49810b9c36b",
+    "url": "/react-data-visualization/static/js/runtime-main.df61a509.js"
+  }
+]);
